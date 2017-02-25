ba748fdbec3c0502e535e795e752f7c7622f1794
Toshal Patel <toshaltpatel@gmail.com>
2017-02-25 22:30:46 +0530

    Update test2.m

--- test2.m
+++ test2.m
@@ -2,23 +2,31 @@
 [y2,Fs2] = wavread('t2.wav');
 T1 = [1/Fs1:1/Fs1:length(y1)/Fs1];
 T2 = [1/Fs2:1/Fs2:length(y2)/Fs2];
+
 figure
 ax1 = subplot(3,1,1);
 ax2 = subplot(3,1,2);
 ax3 = subplot(3,1,3);
+
 plot(ax1, y1, T1);
 plot(ax2, y2, T2);
 plot(ax1, T1, y1);
 plot(ax2, T2, y2);
+
 linkaxes(ax3:ax3, 'x');
 linkaxes(ax1:ax3, 'x');
+
 [C1,lag1] = xcorr[t2,t1];
 [C1,lag1] = xcorr(T2,T1);
 plot(ax3, lag1/Fs1, C1);
+
 linkaxes(ax1:ax3,'x');
+
 T2 = alignsignals(T2,T1);
+
 RMSE = sqrt(mean((T2-T1).^2));
 RMSE
+
 plot(ax1, T1, y1);
 plot(ax3, lag1/Fs1, C1);
  
