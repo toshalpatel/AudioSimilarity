7101623195c84ffe99b1e8c11fa84d456a2fe01f
Toshal Patel <toshaltpatel@gmail.com>
2017-04-13 16:48:48 +0530

    Update intersect1.m

--- intersect/intersect1.m
+++ intersect/intersect1.m
@@ -26,47 +26,11 @@ ax1 = subplot(3,1,1);
 ax2 = subplot(3,1,2);
 ax3 = subplot(3,1,3);
 
->> plot(ax1,t1,y1);
->> plot(ax2,t2,y2);
->> linkaxes(ax1:ax2,'x');
->> [y,Fs] = intersect(y1,y2);
->> t = [1/Fs:1/Fs:length(y)/Fs];
->> plot(ax3,t,y);
-Error using plot
-Vectors must be the same lengths.
- 
->> figure
->> plot(t,y);
-Error using plot
-Vectors must be the same lengths.
- 
->> audiowrite('t3.wav',y,Fs);
-Error using audiowrite>validateFilename (line
-317)
-Cannot create file t3.wav. Permission Denied.
+plot(ax1,t1,y1);
+plot(ax2,t2,y2);
+linkaxes(ax1:ax2,'x');
+[y,Fs] = intersect(y1,y2);
+t = [1/Fs:1/Fs:length(y)/Fs];
 
-Error in audiowrite (line 105)
-[props.filename, fileExisted] =
-validateFilename( props.filename );
- 
->> audiowrite('result.wav',y,Fs);
-Error using audiowrite>validateFilename (line
-317)
-Cannot create file result.wav. Permission
-Denied.
-
-Error in audiowrite (line 105)
-[props.filename, fileExisted] =
-validateFilename( props.filename );
- 
->> filename = 'h.wav';
->> audiowrite(filename,y,Fs);
-Error using audiowrite>validateFilename (line
-317)
-Cannot create file h.wav. Permission Denied.
-
-Error in audiowrite (line 105)
-[props.filename, fileExisted] =
-validateFilename( props.filename );
- 
->> filename = 'C:\h.wav';
+filename = 'h.wav';
+filename = 'C:\h.wav';
